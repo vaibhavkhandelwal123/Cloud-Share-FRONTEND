33bd4d09336aab83bfdfbda26ef93235dc52b625
khandelwalvaibhav123 <khandelwalvaibhav123456@gmail.com>
2025-09-26 10:52:35 +0530

    first all files commit

---
+++ Cloud-Share/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold">Welcome to Cloud Share</h1>
+    </div>
+  )
+}
+
+export default App
